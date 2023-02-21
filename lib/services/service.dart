c934cafb250e789e5269afe89b38bcc5fc7867aa
anasahmed01 <zainkhilji2323@gmail.com>
2023-02-21 15:45:49 +0500

    add_functions

---
+++ lib/services/service.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:crudapi/models/home_model.dart';
+import 'package:http/http.dart' as http;
+
+getUser() async {
+  final url = Uri.parse('https://maaz-api.tga-edu.com/api/users');
+  final getData = await http.get(url);
+  final responseData = jsonDecode(getData.body);
+  return HomeModel.fromJson(responseData);
+}
+
+deleteUser({required id}) async {
+  await http.delete(
+    Uri.parse('https://maaz-api.tga-edu.com/api/users/$id'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  );
+}
+
+addUser(Data model) async {
+  await http.post(
+    Uri.parse("https://maaz-api.tga-edu.com/api/users"),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(model.toJson()),
+  );
+}
+
+updateUser({required Data model, required id}) async {
+  await http.put(
+    Uri.parse("https://maaz-api.tga-edu.com/api/users/$id"),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(model.toJson()),
+  );
+}
