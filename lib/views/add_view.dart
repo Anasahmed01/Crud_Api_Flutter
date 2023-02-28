a493194049fcd0601bbe95c5e29d700b6d3f9639
anasahmed01 <zainkhilji2323@gmail.com>
2023-02-28 17:00:09 +0500

    design_Ui

--- lib/views/add_view.dart
+++ lib/views/add_view.dart
@@ -26,41 +26,80 @@ class _EditViewState extends State<AddView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Center(
+          child: Text(
+            'AddView',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
       body: SafeArea(
-        child: Column(
-          children: [
-            textfield(hintText: 'Enter name', controllar: nameControllar),
-            textfield(
-                hintText: 'Enter username', controllar: userNameControllar),
-            textfield(hintText: 'Enter email', controllar: emailControllar),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(
-                  const Color.fromARGB(255, 37, 27, 147),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                child: Column(
+                  children: [
+                    textfield(
+                        hintText: 'Enter name', controllar: nameControllar),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    textfield(
+                        hintText: 'Enter username',
+                        controllar: userNameControllar),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    textfield(
+                        hintText: 'Enter email', controllar: emailControllar),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                  ],
                 ),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40.0),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.99,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        const Color.fromARGB(255, 0, 0, 0),
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                    onPressed: () async {
+                      setState(() {
+                        Navigator.pop(context);
+                      });
+                      await addUser(
+                        Data(
+                            name: nameControllar.text,
+                            username: userNameControllar.text,
+                            email: emailControllar.text),
+                      );
+                    },
+                    child: const Text(
+                      'Add',
+                      style: TextStyle(fontSize: 20),
+                    ),
                   ),
                 ),
               ),
-              onPressed: () async {
-                setState(() {
-                  Navigator.pop(context);
-                });
-                await addUser(
-                  Data(
-                      name: nameControllar.text,
-                      username: userNameControllar.text,
-                      email: emailControllar.text),
-                );
-              },
-              child: const Text(
-                'Add',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -14,7 +14,9 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff181A20),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
         onPressed: () async {
           await Navigator.push(
             context,
@@ -22,103 +24,131 @@ class _HomeViewState extends State<HomeView> {
           );
           setState(() {});
         },
-        child: const Text("data"),
+        child: const Text(
+          "Add",
+          style: TextStyle(color: Colors.black),
+        ),
       ),
       appBar: AppBar(
-        title: const Text('Crud Oprations'),
-        backgroundColor: const Color.fromARGB(255, 42, 39, 39),
+        title: const Center(
+          child: Text(
+            'Crud Oprations',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       ),
-      body: FutureBuilder(
-          future: getUser(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data.data.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(snapshot.data.data[index].name),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(snapshot.data.data[index].username),
-                          Text(snapshot.data.data[index].email),
-                        ],
-                      ),
-                      trailing: Wrap(
-                        children: [
-                          IconButton(
-                              onPressed: () async {
-                                await deleteUser(
-                                    id: snapshot.data.data[index].id);
-                                setState(() {});
-                              },
-                              icon: const Icon(Icons.delete)),
-                          IconButton(
-                              onPressed: () async {
-                                TextEditingController namesController =
-                                    TextEditingController();
-                                TextEditingController usernamesController =
-                                    TextEditingController();
-                                TextEditingController emailsController =
-                                    TextEditingController();
-                                TextEditingController phonesController =
-                                    TextEditingController();
-                                showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        title: Column(
-                                          children: [
-                                            TextField(
-                                              controller: namesController,
-                                              decoration: const InputDecoration(
-                                                  hintText: 'name'),
-                                            ),
-                                            TextField(
-                                              controller: usernamesController,
-                                              decoration: const InputDecoration(
-                                                  hintText: 'username'),
-                                            ),
-                                            TextField(
-                                              controller: emailsController,
-                                              decoration: const InputDecoration(
-                                                  hintText: 'email'),
-                                            ),
-                                            const SizedBox(
-                                              height: 20,
-                                            ),
-                                            ElevatedButton(
-                                                onPressed: () async {
-                                                  await updateUser(
-                                                      model: Data(
-                                                          name: namesController
-                                                              .text,
-                                                          username:
-                                                              usernamesController
-                                                                  .text,
-                                                          email:
-                                                              emailsController
-                                                                  .text),
-                                                      id: snapshot
-                                                          .data.data[index].id);
-                                                  setState(() {});
-                                                  Navigator.of(context).pop();
-                                                },
-                                                child: const Text("updatedata"))
-                                          ],
-                                        ),
-                                      );
-                                    });
-                              },
-                              icon: const Icon(Icons.edit)),
-                        ],
-                      ),
-                    );
-                  });
-            } else {
-              return const CircularProgressIndicator();
-            }
-          }),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+            future: getUser(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    itemCount: snapshot.data.data.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: ListTile(
+                          shape: RoundedRectangleBorder(
+                            side:
+                                const BorderSide(width: 2, color: Colors.white),
+                            borderRadius:
+                                BorderRadius.circular(10), //<-- SEE HERE
+                          ),
+                          iconColor: const Color.fromARGB(255, 145, 145, 145),
+                          textColor: Colors.white,
+                          title: Text(snapshot.data.data[index].name),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(snapshot.data.data[index].username),
+                              Text(snapshot.data.data[index].email),
+                            ],
+                          ),
+                          trailing: Wrap(
+                            children: [
+                              IconButton(
+                                onPressed: () async {
+                                  TextEditingController namesController =
+                                      TextEditingController();
+                                  TextEditingController usernamesController =
+                                      TextEditingController();
+                                  TextEditingController emailsController =
+                                      TextEditingController();
+                                  showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          backgroundColor: Colors.grey,
+                                          title: Column(
+                                            children: [
+                                              TextField(
+                                                controller: namesController,
+                                                decoration:
+                                                    const InputDecoration(
+                                                        hintText: 'name'),
+                                              ),
+                                              TextField(
+                                                controller: usernamesController,
+                                                decoration:
+                                                    const InputDecoration(
+                                                        hintText: 'username'),
+                                              ),
+                                              TextField(
+                                                controller: emailsController,
+                                                decoration:
+                                                    const InputDecoration(
+                                                        hintText: 'email'),
+                                              ),
+                                              const SizedBox(
+                                                height: 20,
+                                              ),
+                                              ElevatedButton(
+                                                  onPressed: () async {
+                                                    await updateUser(
+                                                        model: Data(
+                                                            name:
+                                                                namesController
+                                                                    .text,
+                                                            username:
+                                                                usernamesController
+                                                                    .text,
+                                                            email:
+                                                                emailsController
+                                                                    .text),
+                                                        id: snapshot.data
+                                                            .data[index].id);
+                                                    setState(() {});
+                                                    Navigator.of(context).pop();
+                                                  },
+                                                  child:
+                                                      const Text("updatedata"))
+                                            ],
+                                          ),
+                                        );
+                                      });
+                                },
+                                icon: const Icon(Icons.edit),
+                              ),
+                              IconButton(
+                                onPressed: () async {
+                                  await deleteUser(
+                                      id: snapshot.data.data[index].id);
+                                  setState(() {});
+                                },
+                                icon: const Icon(Icons.delete),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    });
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }),
+      ),
     );
   }
 }
